5819ef47606d899aae7b32861f01f479b20e36c2
maxim padura <rexpima@gmail.com>
2025-03-14 12:42:04 +0200

    14.03.25db

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(__name__)
-@app.route('/')
-@app.route('/home')
+app = Flask ( __name__ )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy ( app )
+
+
+class Article ( db.Model ):
+    id = db.Column ( db.Integer, primary_key=True )
+    title = db.Column ( db.String ( 100 ), nullable=False )
+    intro = db.Column ( db.String ( 300 ), nullable=False )
+    text = db.Column ( db.Text, nullable=False )
+    date = db.Column ( db.DateTime, default=datetime.utcnow )
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+app = Flask ( __name__ )
+
+
+@app.route ( '/' )
+@app.route ( '/home' )
 def index():
-    return render_template("index.html")
+    return render_template ( "index.html" )
 
-@app.route('/about')
+
+@app.route ( '/about' )
 def about():
-    return render_template("about.html")
+    return render_template ( "about.html" )
 
-@app.route('/user/<string:name>/<int:id>')
+
+@app.route ( '/user/<string:name>/<int:id>' )
 def user(name, id):
-    return "User Page: " + name + " - " + str(id)
+    return "User Page: " + name + " - " + str ( id )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run ( debug=True )
